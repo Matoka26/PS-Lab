fb2ab5230b85f702cb839c60455072d9fb0afdb6
Mihai <mihail_danut24@yahoo.com>
2024-10-05 18:19:35 +0300

    done lab1

--- Lab1/ex2.py
+++ Lab1/ex2.py
@@ -29,7 +29,15 @@ def sinusoidal(t: float, f=1, a=1, phi=0) -> float:
 
 
 def sawtooth(t: float, f=1, a=1, phi=0) -> float:
-    return a * np.abs(2 * np.pi * f * t + phi) % a
+    return 2 * a * np.mod(f * t + phi, a) - a
+
+
+def square(t: float, f=1, a=1, phi=0) -> float:
+    return a * np.sign(np.sin(2 * np.pi * f * t + phi))
+
+
+def s(x: int, y: int, u: float, v: float) -> float:
+    return np.sin(2 * np.pi * (x * u + y * v))
 
 
 # a)
@@ -63,17 +71,52 @@ def ex_b():
 # c)
 def ex_c():
     no_samples = 5000
-    f = 100
+    f = 240
     a = 1
     phi = 0
     samples = np.linspace(0, 1, no_samples)
     plt.plot(samples, sawtooth(samples, f, a, phi), c='r')
     plt.plot([0,0], [a, -a])
     plt.plot(samples, [0]*no_samples)
-    plt.show()
-    # plt.savefig("./figures/ex2_c.pdf")
+    plt.savefig("./figures/ex2_c.pdf")
+
+
+# d)
+def ex_d():
+    no_samples = 5000
+    f = 300
+    a = 1
+    phi = 0
+    samples = np.linspace(0, 1, no_samples)
+    plt.plot(samples, square(samples, f, a, phi), c='r')
+    plt.plot([0,0], [a, -a])
+    plt.plot(samples, [0]*no_samples)
+    plt.savefig("./figures/ex2_d.pdf")
+
+
+# e)
+def ex_e():
+    img = np.random.rand(128, 128)
+    plt.imshow(img, cmap='gray')
+    plt.savefig("./figures/ex2_e.pdf")
+
+
+# f)
+def ex_f():
+    u = 2
+    v = -3
+    img = np.zeros((128, 128))
+    for x in range(len(img)):
+        for y in range(len(img[0])):
+            img[x][y] = s(x, y, u, v)
+    plt.imshow(img, cmap='grey')
+    plt.savefig("./figures/ex2_f.pdf")
+
 
 if __name__ == "__main__":
-   # ex_a()
-   # ex_b()
-    ex_c()
+   ex_a()
+   ex_b()
+   ex_c()
+   ex_d()
+   ex_e()
+   ex_f()
