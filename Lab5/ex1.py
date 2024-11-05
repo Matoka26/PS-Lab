efad30c4457db53e4d3d275960296cdb8c09d90c
Mihai <mihail_danut24@yahoo.com>
2024-11-05 17:36:55 +0200

    done lab5

--- Lab5/ex1.py
+++ Lab5/ex1.py
@@ -27,6 +27,7 @@ depinde acuratet,ea ei.
 '''
 
 '''
+Answers
 a) "din ora-n ora" -> T = 1h = 3600s -> f = 1/3600 Hz
 
 b) Fisierul contine 18288 sample-uri "din ora-n ora"
@@ -37,14 +38,35 @@ c) 0.00013887369981530087
 d) figures/ex1_d.pdf
 
 e) Media semnalului != 0 -> contine o componenta continua
+    figures/ex1_e.pdf
+
+f) Biggest 4 frequencies: [0.00013883 0.00013884 0.00013886 0.00013887]
+
+g) figures/ex1_g.pdf
+
+h) 
+    If we know the sample rate, like in this example, we can determine larger time units such as days and weeks.
+    We can seek for patterns in:
+        - the day, such as low traffic at night
+        - the week, low traffic in the weekends
+    Maybe we can observe a behaviour in the months, like low traffic in the summers or winters
+    
+    Problems:
+        The patterns are pure assumptions with no evidence to support them and the found patterns might
+        apply to different times, for example, the assumption of low traffic in the weekend might be 
+        actually low traffic Mondey and Tuesday
+    
+i) figures/ex1_i.pdf
 '''
 
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 dataset_path = './datasets/Train.csv'
 figures_directory = './figures'
+
 
 if __name__ == "__main__":
     if not os.path.isdir(figures_directory):
@@ -54,12 +76,16 @@ if __name__ == "__main__":
     sample_rate = 1/3600
     samples = x[:, :1][1:].flatten()
     wave = x[:, 2:3][1:].flatten()
+
+    date_times = pd.read_csv(dataset_path, parse_dates=["Datetime"], dayfirst=True)
+
     fft_components = np.fft.fft(wave)
     fft_frequencies = np.fft.fftfreq(wave.shape[0], 1/sample_rate)
 
+
     # c)
     # # 0.00013887369981530087
-    # print(np.max(fft_frequencies))
+    print(f'c) Max frequency: {np.max(fft_frequencies)}')
 
     # d)
     # plot transformation
@@ -90,7 +116,61 @@ if __name__ == "__main__":
     plt.plot(samples, wave-dc)
     plt.grid(True)
     plt.title('Normalized wave')
-    plt.savefig(f"./{figures_directory}/ex1_de.pdf")
+    plt.savefig(f"./{figures_directory}/ex1_e.pdf")
     plt.clf()
 
     # f)
+    nof_first_components = 4
+    time_period = {
+        "day": 3600 * 24,
+        "week": 3600 * 24 * 7,
+        "month": 3600 * 24 * 30,
+        "year": 3600 * 24 * 365
+    }
+
+    normalized_fft = np.fft.fft(wave-dc)
+    fft_frequencies = np.fft.fftfreq(wave.shape[0], 1 / sample_rate)[: len(normalized_fft)//2]
+    sorted_magnitudes = np.argsort(fft_frequencies)
+
+    top_magnitudes = fft_frequencies[sorted_magnitudes][-nof_first_components:]
+    print(f'f) Biggest {nof_first_components} frequencies: {top_magnitudes}')
+
+    for time in time_period:
+        print(f'By {time}: {top_magnitudes * time_period[time]}')
+
+    # g)
+    nof_samples = 1111
+    times = date_times.iloc[nof_samples:]
+    monday_index = times[times["Datetime"].dt.weekday == 0].index[24]
+    plt.plot(samples[monday_index:monday_index + nof_samples], wave[monday_index: monday_index + nof_samples])
+    plt.grid(True)
+    plt.title("One Month of Traffic from Monday")
+    plt.savefig(f"./{figures_directory}/ex1_g.pdf")
+    plt.clf()
+
+    # h) answers are up
+    # i)
+    wave = wave[:750]
+    samples = samples[:750]
+    fft_components = np.fft.fft(wave)
+    threshold = np.average(np.abs(fft_components)) * 1.5
+    fft_filtered = [0+0j if np.abs(comp) < threshold else comp for comp in fft_components]
+
+    filtered_signal = np.real(np.fft.ifft(fft_filtered))
+
+    fig, ax = plt.subplots(nrows=2, ncols=1)
+    y_max = max(np.max(np.abs(filtered_signal[:len(fft_filtered) // 2])), np.max(np.abs(wave[:len(fft_filtered) // 2])))
+
+    ax[1].plot(samples[:len(samples)//2], filtered_signal[:len(fft_filtered)//2])
+    ax[1].set_ylim(0, y_max)
+    ax[1].grid(True)
+    ax[1].set_title("Filtered signal")
+
+    ax[0].plot(samples[:len(samples)//2], wave[:len(fft_filtered)//2])
+    ax[0].set_ylim(0, y_max)
+    ax[0].grid(True)
+    ax[0].set_title("Initial signal")
+    fig.tight_layout()
+    fig.savefig(f"./{figures_directory}/ex1_i.pdf")
+
+    plt.clf()
